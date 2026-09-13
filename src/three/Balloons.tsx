e8aef476d7f4af74e350276ab6e24f317c807666
Timon Biswas <aloneboy0022.ti@gmail.com>
2026-09-13 21:48:48 +0600

    fix(webgl): add missing useEffect import in Balloons.tsx to fix build error

--- src/three/Balloons.tsx
+++ src/three/Balloons.tsx
@@ -1,4 +1,4 @@
-import { useRef, useMemo, useCallback } from 'react';
+import { useRef, useMemo, useCallback, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { useStoryStore } from '../store/useStoryStore';
